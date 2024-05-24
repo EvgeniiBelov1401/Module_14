a9d1ee622ef49b5f286dce4d20eff642edc283ed
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-24 14:12:37 +0300

    Exercise-13-00-OrderBy

---
+++ Module-14/Exercise-13-00-OrderBy/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise_13_00_OrderBy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            var numbers=new List<int>();
+
+            for(int i = 0; i < random.Next(100); i++)
+            {
+                numbers.Add(random.Next(100));
+            }
+
+            var orderedNumbers = numbers.OrderBy(n => n);
+
+            foreach(var num  in orderedNumbers)
+            {
+                Console.Write(num+" ");
+            }
+        }
+    }
+}
