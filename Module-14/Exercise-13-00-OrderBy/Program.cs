3a051fdba36a3500461a50f86bf04c893260e36a
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-24 14:27:54 +0300

    проверил несколько методов расширения LINQ

--- Module-14/Exercise-13-00-OrderBy/Program.cs
+++ Module-14/Exercise-13-00-OrderBy/Program.cs
@@ -4,20 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-
-            var numbers=new List<int>();
-
-            for(int i = 0; i < random.Next(100); i++)
+            while (true)
             {
-                numbers.Add(random.Next(100));
-            }
+                Random random = new Random();
 
-            var orderedNumbers = numbers.OrderBy(n => n);
+                var numbers = new List<int>();
 
-            foreach(var num  in orderedNumbers)
-            {
-                Console.Write(num+" ");
+                for (int i = 0; i < random.Next(100); i++)
+                {
+                    numbers.Add(random.Next(100));
+                }
+                #region OrderBy
+                var orderedNumbers = numbers.OrderBy(n => n);
+
+                foreach (var num in orderedNumbers)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+                #endregion
+
+                #region OrderByDescending
+                var orderByDesNumbers = numbers.OrderByDescending(n => n);
+                foreach (var num in orderByDesNumbers)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+                #endregion
+
+                #region Where
+                var evenNumbers = numbers.Where(n => n % 2 == 0);
+                foreach (var num in evenNumbers)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+                #endregion
+
+                #region All
+                if (numbers.All(n => n < 90))
+                {
+                    Console.WriteLine("Все элементы меньше 90");
+                }
+                else
+                {
+                    Console.WriteLine("Есть элементы, которые >= 90");
+                }
+                #endregion
+
+
+
+
+                Console.ReadLine();
+                Console.Clear();
             }
         }
     }
