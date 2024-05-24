d189d03dbfa752ffd13dd7d07527a263046022df
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-24 15:14:11 +0300

    Exercise-14-00-008

---
+++ Module-14/Exercise-14-00-008/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exercise_14_00_008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var objects = new List<object>()
+            {
+               1,
+               "Сергей ",
+               "Андрей ",
+               300,
+            };
+            #region выражения LINQ
+            var selectName = (from n in objects
+                              where n is string
+                              orderby n
+                              select n);
+
+            foreach (var obj in selectName)
+            {
+                Console.WriteLine(obj);
+            }
+            #endregion
+
+            #region методы-расширения
+            var selectName2=objects.Where(n => n is string).OrderBy(n => n);
+
+            foreach (var obj in selectName2)
+            {
+                Console.WriteLine(obj);
+            }
+            #endregion
+        }
+    }
+}
