fa1fd9b9ea83c57f054d4dc22e6606447132749b
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-24 13:55:31 +0300

    Exercise-14-00-004

---
+++ Module-14/Exercise-14-00-004/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercise_14_00_004
+{
+    //Задача: выбрать имена на букву А и отсортировать в алфавитном порядке.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+
+            var peopleOnA=new List<string>();
+
+            foreach(string person in people)
+            {
+                var personToChar=person.ToCharArray();
+                if (personToChar[0] == 'А')
+                {
+                    peopleOnA.Add(person);
+                }
+            }
+            peopleOnA.Sort();
+
+            foreach(string person in peopleOnA)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
