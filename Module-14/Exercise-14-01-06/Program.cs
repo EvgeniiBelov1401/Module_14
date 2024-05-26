0e2e291a91c10113326695c978969ffdaa5ca009
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-26 14:30:00 +0300

    Exercise-14-01-06

---
+++ Module-14/Exercise-14-01-06/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise_14_01_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numsList = new List<int[]>()
+            {
+               new[] {2, 3, 7, 1},
+               new[] {45, 17, 88, 0},
+               new[] {23, 32, 44, -6},
+            };
+
+            //var orderedList = numsList.SelectMany(s => s).OrderBy(s => s);
+
+            foreach (var item in numsList.SelectMany(s => s).OrderBy(s => s))
+                Console.WriteLine(item);
+        }
+    }
+}
