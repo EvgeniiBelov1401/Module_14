34116dc7b785fa86ec2e1358fa94f8b828c66b8b
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-26 13:54:38 +0300

    Exercise-14-01-02

---
+++ Module-14/Exercise-14-01-02/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise_14_01_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = { "Раз два три", "четыре пять шесть","семь восемь девять" };
+
+            var words = from item in text
+                        from word in item.Split(' ')
+                        select word;
+            
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+            foreach (var item in text)
+            {
+                foreach (var word in item.Split(' '))
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
