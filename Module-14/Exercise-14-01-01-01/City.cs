4330f7eba43080144c7d9fe22aa98d9fc3a0fb35
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-26 13:28:42 +0300

    Exercise-14-01-01-01

---
+++ Module-14/Exercise-14-01-01-01/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_14_01_01_01
+{
+    internal class City
+    {
+        public string Name { get; set; }
+        public long Population { get; set; }
+
+        public City(string name, int population)
+        {
+            Name = name;
+            Population = population;
+        }
+    }
+}
---
+++ Module-14/Exercise-14-01-01-01/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Exercise_14_01_01_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Countries=new Dictionary<string, List<City>>();
+            var RussianCities=new List<City>();
+            var BelarusCities=new List<City>();
+            var AmericanCities =new List<City>();
+
+            RussianCities.Add(new City("Москва", 11900000));
+            RussianCities.Add(new City("Санкт-Петербург", 4991000));
+            RussianCities.Add(new City("Волгоград", 1099000));
+            RussianCities.Add(new City("Казань", 1169000));
+            RussianCities.Add(new City("Севастополь", 449138));
+            Countries.Add("Россия",RussianCities);
+
+
+            BelarusCities.Add(new City("Минск", 1200000));
+            BelarusCities.Add(new City("Витебск", 362466));
+            BelarusCities.Add(new City("Гродно", 368710));
+            Countries.Add("Беларусь", BelarusCities);
+
+
+            AmericanCities.Add(new City("Нью-Йорк", 8399000));
+            AmericanCities.Add(new City("Вашингтон", 705749));
+            AmericanCities.Add(new City("Альбукерке", 560218));
+            Countries.Add("США", AmericanCities);
+
+
+            foreach (var country in Countries)
+            {
+                foreach (var city in country.Value.Where(x=>x.Population>1000000))
+                {
+                    Console.WriteLine(city.Name);
+                }
+            }
+        }
+    }
+}
