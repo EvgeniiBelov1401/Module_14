4ffdcdcfecb7e81ebcd5c38818a331f264082678
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-26 18:29:38 +0300

    Exercise-14-02-01

---
+++ Module-14/Exercise-14-02-01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise_14_02_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "Обезьяна", "Лягушка", "Кот", "Собака", "Черепаха" };
+
+            var animals = words.Select(x => new
+            {
+                Name = x,
+                Length = x.Length
+            }).OrderBy(x=>x.Length);
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"{animal.Name} - {animal.Length} букв");
+            }
+        }
+    }
+}
