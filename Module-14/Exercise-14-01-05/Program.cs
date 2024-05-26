5f712e18203a4d5a145d51cdc2ba81020dab3df4
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-26 14:16:41 +0300

    Exercise-14-01-05

---
+++ Module-14/Exercise-14-01-05/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise_14_01_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var companies = new Dictionary<string, string[]>();
+
+            companies.Add("Apple", new[] { "Mobile", "Desktop" });
+            companies.Add("Samsung", new[] { "Mobile" });
+            companies.Add("IBM", new[] { "Desktop" });
+
+            foreach (var company in companies.Where(x=>x.Value.Contains("Mobile")) ) 
+            {
+               Console.WriteLine(company.Key);
+            }
+        }
+    }
+}
