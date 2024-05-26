9a594c2f85335c4d5cbd46a3de56da826f765b63
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-26 12:27:17 +0300

    Exercise-14-01-01

---
+++ Module-14/Exercise-14-01-01/City.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_14_01_01
+{
+    internal class City
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+
+        public City(string name,int population)
+        {
+            Name = name;
+            Population = population;
+        }
+
+    }
+}
---
+++ Module-14/Exercise-14-01-01/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercise_14_01_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var russianCities = new List<City>();
+
+            russianCities.Add(new City("Москва", 11900000));
+            russianCities.Add(new City("Санкт-Петербург", 4991000));
+            russianCities.Add(new City("Волгоград", 1099000));
+            russianCities.Add(new City("Казань", 1169000));
+            russianCities.Add(new City("Севастополь", 449138));
+
+            foreach (var city in russianCities.Where(x=>x.Population>1000000).OrderByDescending(x=>x.Population))
+            {
+                Console.WriteLine(city.Name+": "+city.Population);
+            }
+            Console.ReadLine();
+
+            foreach (var city in russianCities.Where(x=>x.Name.Length<=10).OrderBy(x=>x.Name.Length))
+            {
+                Console.WriteLine(city.Name);
+            }
+        }
+    }
+}
